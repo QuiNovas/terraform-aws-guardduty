847a653be191d236e31f4ec8d8b2f591134845ca
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2018-09-11 16:08:18 +0530

    added guardduty iam policies

--- main.tf
+++ main.tf
@@ -1,3 +1,52 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_service_linked_role" "guardduty" {
+  aws_service_name = "guardduty.amazonaws.com"
+}
+
+resource "aws_iam_policy" "guardduty_full_access" {
+  name        = "guardduty-full-access"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+               "guardduty:*"
+            ],
+            "Resource": "*"
+        },      
+        {
+            "Effect": "Allow",
+            "Action": [
+                "iam:CreateServiceLinkedRole"
+            ],
+            "Resource": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/guardduty.amazonaws.com/AWSServiceRoleForAmazonGuardDuty",
+            "Condition": {
+                "StringLike": {
+                    "iam:AWSServiceName": "guardduty.amazonaws.com"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "iam:PutRolePolicy",
+                "iam:DeleteRolePolicy"
+            ],
+            "Resource": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/guardduty.amazonaws.com/AWSServiceRoleForAmazonGuardDuty"
+        }
+    ]
+} 
+EOF
+}
+
+resource "aws_iam_policy_attachment" "guardduty" {
+  name       = "guardduty-attachment"
+  roles      = ["${aws_iam_service_linked_role.guardduty.name}"]
+  policy_arn = "${aws_iam_policy.guardduty_full_access.arn}"
+}
 resource "aws_guardduty_detector" "master" {
   enable = "${var.enable}"
 }
