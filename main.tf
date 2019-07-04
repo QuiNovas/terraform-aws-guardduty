a64e3566ffcf2ac57d52f697e148a48bcf767e62
rajhansh-seyfert <kumarrajhanshgit@gmail.com>
2019-07-04 19:10:26 +0530

    updated to terraform 12

--- main.tf
+++ main.tf
@@ -1,46 +1,47 @@
 resource "aws_guardduty_detector" "master" {
-  enable = "${var.enable}"
+  enable = var.enable
 }
 
 resource "aws_s3_bucket_object" "MyThreatIntelSet" {
-  count                  = "${var.threat_intel_list_path == "" ? 0 : 1}"
-  bucket                 = "${aws_s3_bucket.guard_duty_lists.id}"
-  key                    = "MyThreatIntelSet.txt"
-  source                 = "${var.threat_intel_list_path}"
-  etag                   = "${md5(file("${var.threat_intel_list_path}"))}"
+  count  = var.threat_intel_list_path == "" ? 0 : 1
+  bucket = aws_s3_bucket.guard_duty_lists[0].id
+  key    = "MyThreatIntelSet.txt"
+  source = var.threat_intel_list_path
+  etag   = filemd5(var.threat_intel_list_path)
 }
 
 resource "aws_guardduty_threatintelset" "MyThreatIntelSet" {
-  count       = "${var.threat_intel_list_path== "" ? 0 : 1}"
-  activate    = "${var.threat_intel_set_active}"
-  detector_id = "${aws_guardduty_detector.master.id}"
-  format      = "${var.threat_intel_set_format}"
-  location    = "https://s3.amazonaws.com/${aws_s3_bucket_object.MyThreatIntelSet.bucket}/${aws_s3_bucket_object.MyThreatIntelSet.key}"
+  count       = var.threat_intel_list_path == "" ? 0 : 1
+  activate    = var.threat_intel_set_active
+  detector_id = aws_guardduty_detector.master.id
+  format      = var.threat_intel_set_format
+  location    = "https://s3.amazonaws.com/${aws_s3_bucket_object.MyThreatIntelSet[0].bucket}/${aws_s3_bucket_object.MyThreatIntelSet[0].key}"
   name        = "MyThreatIntelSet"
 }
 
 resource "aws_s3_bucket_object" "MyIPSet" {
-  count                  = "${var.ip_set_list_path == "" ? 0 : 1}"
-  bucket                 = "${aws_s3_bucket.guard_duty_lists.id}"
-  key                    = "MyIPSet.txt"
-  source                 = "${var.ip_set_list_path}"
-  etag                   = "${md5(file("${var.ip_set_list_path}"))}"
+  count  = var.ip_set_list_path == "" ? 0 : 1
+  bucket = aws_s3_bucket.guard_duty_lists[0].id
+  key    = "MyIPSet.txt"
+  source = var.ip_set_list_path
+  etag   = filemd5(var.ip_set_list_path)
 }
 
 resource "aws_guardduty_ipset" "MyIPSet" {
-  count       = "${var.ip_set_list_path == "" ? 0 : 1}"
-  activate    = "${var.ip_set_active}"
-  detector_id = "${aws_guardduty_detector.master.id}"
-  format      = "${var.ip_set_format}"
-  location    = "https://s3.amazonaws.com/${aws_s3_bucket_object.MyIPSet.bucket}/${aws_s3_bucket_object.MyIPSet.key}"
+  count       = var.ip_set_list_path == "" ? 0 : 1
+  activate    = var.ip_set_active
+  detector_id = aws_guardduty_detector.master.id
+  format      = var.ip_set_format
+  location    = "https://s3.amazonaws.com/${aws_s3_bucket_object.MyIPSet[0].bucket}/${aws_s3_bucket_object.MyIPSet[0].key}"
   name        = "MyIPSet"
 }
 
 resource "aws_guardduty_member" "members" {
-  count              = "${var.member_list_count}"
-  account_id         = "${lookup(var.member_list[count.index], "account_id")}"
-  detector_id        = "${aws_guardduty_detector.master.id}"
-  email              = "${lookup(var.member_list[count.index], "member_email")}"
-  invite             = "${lookup(var.member_list[count.index], "invite")}"
+  count              = var.member_list_count
+  account_id         = var.member_list[count.index]["account_id"]
+  detector_id        = aws_guardduty_detector.master.id
+  email              = var.member_list[count.index]["member_email"]
+  invite             = var.member_list[count.index]["invite"]
   invitation_message = "please accept guardduty invitation"
 }
+
