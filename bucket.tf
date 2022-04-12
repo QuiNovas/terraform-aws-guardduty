78f45520c76034fcb56b6e3d2a18096e1ecdc06e
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2022-04-12 08:50:38 -0400

    update reference to conditional bucket, replace s3 bucket object

--- bucket.tf
+++ bucket.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket" "guard_duty_lists" {
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "guard_duty_lists_encryption" {
-  bucket = aws_s3_bucket.guard_duty_lists.bucket
+  bucket = aws_s3_bucket.guard_duty_lists.0.bucket
 
   rule {
     apply_server_side_encryption_by_default {
@@ -19,7 +19,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "guard_duty_lists_
 }
 
 resource "aws_s3_bucket_versioning" "guard_duty_lists_versioning" {
-  bucket = aws_s3_bucket.guard_duty_lists.bucket
+  bucket = aws_s3_bucket.guard_duty_lists.0.bucket
 
   versioning_configuration {
     status = "Enabled"
@@ -27,7 +27,7 @@ resource "aws_s3_bucket_versioning" "guard_duty_lists_versioning" {
 }
 
 resource "aws_s3_bucket_logging" "guard_duty_lists_logging" {
-  bucket = aws_s3_bucket.guard_duty_lists.bucket
+  bucket = aws_s3_bucket.guard_duty_lists.0.bucket
 
   target_bucket = data.aws_s3_bucket.log_bucket[0].id
   target_prefix = "s3/guard_duty/"
@@ -35,7 +35,7 @@ resource "aws_s3_bucket_logging" "guard_duty_lists_logging" {
 
 
 data "aws_iam_policy_document" "guard_duty_lists" {
-  count  = local.bucket_creation_count
+  count = local.bucket_creation_count
   statement {
     actions = [
       "s3:*",
--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_guardduty_detector" "master" {
   enable = var.enable
 }
 
-resource "aws_s3_bucket_object" "MyThreatIntelSet" {
+resource "aws_s3_object" "MyThreatIntelSet" {
   count  = var.threat_intel_list_path == "" ? 0 : 1
   bucket = aws_s3_bucket.guard_duty_lists[0].id
   key    = "MyThreatIntelSet.txt"
@@ -15,11 +15,11 @@ resource "aws_guardduty_threatintelset" "MyThreatIntelSet" {
   activate    = var.threat_intel_set_active
   detector_id = aws_guardduty_detector.master.id
   format      = var.threat_intel_set_format
-  location    = "https://s3.amazonaws.com/${aws_s3_bucket_object.MyThreatIntelSet[0].bucket}/${aws_s3_bucket_object.MyThreatIntelSet[0].key}"
+  location    = "https://s3.amazonaws.com/${aws_s3_object.MyThreatIntelSet[0].bucket}/${aws_s3_object.MyThreatIntelSet[0].key}"
   name        = "MyThreatIntelSet"
 }
 
-resource "aws_s3_bucket_object" "MyIPSet" {
+resource "aws_s3_object" "MyIPSet" {
   count  = var.ip_set_list_path == "" ? 0 : 1
   bucket = aws_s3_bucket.guard_duty_lists[0].id
   key    = "MyIPSet.txt"
@@ -32,7 +32,7 @@ resource "aws_guardduty_ipset" "MyIPSet" {
   activate    = var.ip_set_active
   detector_id = aws_guardduty_detector.master.id
   format      = var.ip_set_format
-  location    = "https://s3.amazonaws.com/${aws_s3_bucket_object.MyIPSet[0].bucket}/${aws_s3_bucket_object.MyIPSet[0].key}"
+  location    = "https://s3.amazonaws.com/${aws_s3_object.MyIPSet[0].bucket}/${aws_s3_object.MyIPSet[0].key}"
   name        = "MyIPSet"
 }
 
